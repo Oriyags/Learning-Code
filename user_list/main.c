54e5d7616e0fb5a6cf8a6c7a2d3ec59afdfd60d8
Eylon <eylon462@gmail.com>
2023-10-30 13:43:59 +0200

    UserList CR from Eylon <3

--- user_list/main.c
+++ user_list/main.c
@@ -9,6 +9,11 @@ int main() {
 
     printf(ANSI_COLOR_GREEN "Welcome to the List Manipulation Program!\n" ANSI_COLOR_RESET);
 
+/* 
+[CR] Already told you this in the last CR: this function (run_list_program) should not exist at all, "run list program" is what the main function do, its whole purpose.
+The main function should have the main program loop (while(1)) that calls other functions and utils.
+The code from run_list_program SHOULD be in the main function.
+*/
     run_list_program(&my_list);
 
     clear_list(&my_list); // Clear the list before exiting
